75043084b9edc06a97ab51fb6f06d3333e2960b1
muppettMP <rowerybikelpl@gmail.com>
2021-11-25 13:17:43 +0100

    Add navigation toggle mechanism

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+const toolsBtn = document.getElementById("nav-tools-open-btn");
+const modulesBtn = document.getElementById("nav-modules-open-btn");
+const navTools = document.getElementById("nav-tools");
+const navModules = document.getElementById("nav-modules");
 
+// initially remove hide from tools for development
+// navTools.classList.remove("hide");
+
+toolsBtn.addEventListener("click", (e) => {
+  toggleNavTools();
+  hideNavModules();
+});
+
+modulesBtn.addEventListener("click", (e) => {
+  toggleNavModules();
+  hideNavTools();
+});
+
+function toggleNavTools() {
+  if (navTools.classList.contains("hide")) {
+    navTools.classList.remove("hide");
+  } else {
+    navTools.classList.add("hide");
+  }
+}
+
+function hideNavTools() {
+  if (!navTools.classList.contains("hide")) {
+    navTools.classList.add("hide");
+  }
+}
+
+function toggleNavModules() {
+  if (navModules.classList.contains("hide")) {
+    navModules.classList.remove("hide");
+  } else {
+    navModules.classList.add("hide");
+  }
+}
+
+function hideNavModules() {
+  if (!navModules.classList.contains("hide")) {
+    navModules.classList.add("hide");
+  }
+}
